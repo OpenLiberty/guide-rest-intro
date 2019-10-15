b7e0a9e1851946d8d12ff65323a82d6ccc0fa6a7
Gilbert Kwan <gkwan@ca.ibm.com>
2019-10-15 13:22:47 -0400

    Update travisTest.sh

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -7,12 +7,14 @@ set -euxo pipefail
 ##
 ##############################################################################
 
-mvn -q clean package liberty:create liberty:install-feature liberty:deploy
-mvn -q liberty:start
+mvn clean package liberty:create liberty:install-feature liberty:deploy > create.log
+mvn liberty:start > start.log
 mvn failsafe:integration-test liberty:stop > out.log
 ERROR=`grep ERROR out.log | wc -l`
 if [ $ERROR -ne 0 ]; then
     mvn liberty:stop
+    cat create.log
+    cat start.log
     cat out.log
     echo "Test Failed!"
     #exit 1
