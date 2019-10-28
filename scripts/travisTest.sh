e5c68c204ea2d5d17e08f95bbbafd57769f0d790
Gilbert Kwan <gkwan@ca.ibm.com>
2019-10-28 17:20:15 -0400

    Update travisTest.sh

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -10,7 +10,7 @@ set -euxo pipefail
 mvn -q clean package liberty:create liberty:install-feature liberty:deploy
 mvn liberty:start > start.log
 mvn failsafe:integration-test liberty:stop > out.log
-ERROR=`grep ERROR out.log | wc -l` | awk '{$1=$1};1'
+ERROR=`grep ERROR out.log | wc -l | awk '{$1=$1};1'`
 if [ "$ERROR" -ne "0" ]; then
     mvn liberty:stop
     cat ~/.m2/settings.xml
