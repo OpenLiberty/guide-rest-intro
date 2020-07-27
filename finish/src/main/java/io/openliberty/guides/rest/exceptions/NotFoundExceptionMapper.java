17cb99691a7611a725dfd893a44834f7bf926f99
Austin Seto <austin.seto@ibm.com>
2020-07-27 14:19:16 -0400

    Exception mapper for NotFoundException

---
+++ finish/src/main/java/io/openliberty/guides/rest/exceptions/NotFoundExceptionMapper.java
@@ -0,0 +1,23 @@
+package io.openliberty.guides.rest.exceptions;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+// tag::generic[]
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
+// end::generic[]
+    
+    @Override
+    public Response toResponse(NotFoundException ex) {
+        ErrorResponse response = new ErrorResponse("404", ex.getMessage());
+        return Response.status(Response.Status.NOT_FOUND)
+                       .entity(response)
+                       .type(MediaType.APPLICATION_JSON)
+                       .build();
+    }
+
+}
