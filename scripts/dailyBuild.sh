c07a156db70a54dd5a267933c296ecfdd3ab1ada
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2021-01-08 14:39:49 -0500

    Add daily build to GH Actions (#146)
    * daily build update
    
    * add dailyBuild script
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml
    
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>

---
+++ scripts/dailyBuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+while getopts t:d:b:u: flag; do
+    case "${flag}" in
+    t) DATE="${OPTARG}" ;;
+    d) DRIVER="${OPTARG}" ;;
+    b) BUILD="${OPTARG}" ;;
+    u) DOCKER_USERNAME="${OPTARG}" ;;
+    esac
+done
+
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" pom.xml
+cat pom.xml
+
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_USERNAME"/olguides:"$BUILD";g" Dockerfile
+cat Dockerfile
+
+../scripts/testApp.sh
